ea27b3a940865b9b5bf79c93e4240fda29f01b70
azk4n <azk4n@null.net>
2016-03-23 13:52:45 -0300

    updating..

---
+++ uri1016.py
@@ -0,0 +1,2 @@
+distance = int(raw_input())
+print "%d minutos" %(150 / 60 * distance)
---
+++ uri1017.py
@@ -0,0 +1,4 @@
+time_travel = int(raw_input())
+average_speed = int(raw_input())
+
+print "%.3f" %((time_travel * average_speed) / 12.0 )
